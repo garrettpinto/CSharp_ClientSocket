71a21c1a9960f8e3fd412ab20773e843b1a91bb5
Garrett Pinto <garrettpinto@gmail.com>
2023-04-09 19:17:28 -0400

    initial

---
+++ ClientSocket/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientSocket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int BUFFER_SIZE = 20248;
+            IPAddress server_ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipe = new IPEndPoint(server_ip, 1234);
+
+
+            Socket cs = new Socket(server_ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            cs.Connect(ipe);
+
+            string msg;
+            byte[] b = new byte[BUFFER_SIZE];
+
+            // abcdefgh (first message)
+            // BB (second message)
+            // BBcdefgh
+
+            Array.Clear(b, 0, b.Length);
+
+            cs.Receive(b);
+
+            msg = Encoding.ASCII.GetString(b).TrimEnd('\0');
+            Console.WriteLine("[+] Received from server; {0}",msg);
+
+            cs.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
